1de7e170c30f376eeb085e4d0712c0dc85ef0ba6
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-03-01 11:19:44 +0000

    config: Enable NEED_DMA_MAP_STATE by default when SWIOTLB is selected

--- src/github.com/blitzrk/qap-project/dat/dat.go
+++ src/github.com/blitzrk/qap-project/dat/dat.go
@@ -12,4 +12,4 @@ import (
 // whitespace between row entries and newlines between rows
 func Read(file []byte) []matrix.Matrix {
 	data := make([]matrix.Matrix, 0)
-	for _, mat := range bytes.Split
+	for _, mat := range bytes.Split(file, []byte("
