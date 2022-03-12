46999b9c4c68d1e05df6377ad1d243e4b7d66637
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-03-12 10:37:57 +0000

    changing names

--- src/github.com/blitzrk/qap-project/dat/dat.go
+++ src/github.com/blitzrk/qap-project/dat/dat.go
@@ -14,4 +14,5 @@ func Read(file []byte) []matrix.Matrix {
 	data := make([]matrix.Matrix, 0)
 	for _, mat := range bytes.Split(file, []byte("\n\n")) {
 		temp := make(matrix.Matrix, 0)
-		didParse := 
+		didParse := true
+	Elem
