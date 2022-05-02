a4dfe7cfe41a8a8d61630bdf2c8e1cb5c23f1fa5
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-05-02 19:11:01 +0000

    rtnetlink: Mask the rta_type when range checking

--- src/github.com/blitzrk/qap-project/data/data.go
+++ src/github.com/blitzrk/qap-project/data/data.go
@@ -1,2 +1,17 @@
 
 package data
+
+import (
+	"fmt"
+	"github.com/blitzrk/qap-project/matrix"
+	"math"
+	"math/rand"
+)
+
+type generator struct {
+	n      int
+	fscale float64
+}
+
+func New(n int, fscale float64) *generator {
+	return &generator{n, fscale}
