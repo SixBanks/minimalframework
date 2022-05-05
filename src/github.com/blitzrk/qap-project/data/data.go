1f6c76689b527116821f5d54c4e0568ff6f72113
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-05-05 06:11:38 +0000

    demo link

--- src/github.com/blitzrk/qap-project/data/data.go
+++ src/github.com/blitzrk/qap-project/data/data.go
@@ -15,3 +15,16 @@ type generator struct {
 
 func New(n int, fscale float64) *generator {
 	return &generator{n, fscale}
+}
+
+func (g *generator) Distance() (matrix.Matrix, error) {
+	m := matrix.Matrix(make([][]matrix.Element, g.n))
+
+	for i := 0; i < g.n; i++ {
+		m[i] = make([]matrix.Element, g.n)
+		for j := 0; j < g.n; j++ {
+			m[i][j] = matrix.Element(rand.Float64())
+		}
+	}
+
+	return m, nil
