80ea8eb5c193311e99b22c1e59eafed9575a8f21
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-09-20 10:26:15 +0000

    removes console.log

--- src/github.com/blitzrk/qap-project/main.go
+++ src/github.com/blitzrk/qap-project/main.go
@@ -73,4 +73,5 @@ loop:
 				fmt.Println(res.Score, res.Perm)
 			}
 		case <-completed:
-			// Bug: may lose last few solutions due to race conditio
+			// Bug: may lose last few solutions due to race condition
+			fmt.Pr
