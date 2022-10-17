10741ff894b4cbfe7f26e2a50c9f01d03a7e0ee1
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-10-17 09:28:16 +0000

    aesthetics

--- src/github.com/blitzrk/qap-project/matrix/math.go
+++ src/github.com/blitzrk/qap-project/matrix/math.go
@@ -9,3 +9,13 @@ func (m Matrix) Apply(f func(Element) Element) Matrix {
 		res[ri] = make([]Element, len(r))
 		for ci, c := range r {
 			res[ri][ci] = f(c)
+		}
+	}
+	return res
+}
+
+func (m Matrix) Sum() float64 {
+	var s float64
+	for _, r := range m {
+		for _, c := range r {
+			s += float64(c)
