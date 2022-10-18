86f96fea4b0289ac9e7240d09afcf937e71f27a7
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-10-18 11:16:25 +0000

    ARM: OMAP2+: Fix dmtimer set source clock failure

--- src/github.com/blitzrk/qap-project/matrix/matrix.go
+++ src/github.com/blitzrk/qap-project/matrix/matrix.go
@@ -5,3 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"strings"
+)
+
+var (
+	SizeMismatchError = errors.New("Matrix sizes do not match")
+	NotSquareError    = errors.New("One or more matrices are not square")
+)
+
+type Element float64
