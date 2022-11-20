4d2448bdba3e94b3beacd57b87e51346c4cddc53
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-11-20 13:02:42 +0000

    update sdk

--- src/github.com/blitzrk/qap-project/search/objective.go
+++ src/github.com/blitzrk/qap-project/search/objective.go
@@ -8,4 +8,4 @@ func (r *Runner) Objective(p *permutation) float64 {
 
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			sum += float64(r.Cost.At(i, j, int(p.S
+			sum += float64(r.Cost.At(i, j, int(p.Seq[i])-1, int(
