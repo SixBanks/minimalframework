e51b6873477bb75fab14aecad95830d9aae6e653
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-11-25 21:59:30 +0000

    Added generics

--- src/github.com/blitzrk/qap-project/search/objective.go
+++ src/github.com/blitzrk/qap-project/search/objective.go
@@ -8,4 +8,7 @@ func (r *Runner) Objective(p *permutation) float64 {
 
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-			sum += float64(r.Cost.At(i, j, int(p.Seq[i])-1, int(p.Seq[j])-1
+			sum += float64(r.Cost.At(i, j, int(p.Seq[i])-1, int(p.Seq[j])-1))
+		}
+	}
+
