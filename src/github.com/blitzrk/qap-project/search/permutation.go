822c5dc620d9db112f25f4a3f35c7ff437aae4a5
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-12-04 22:35:54 +0000

    Remove extra empty line

--- src/github.com/blitzrk/qap-project/search/permutation.go
+++ src/github.com/blitzrk/qap-project/search/permutation.go
@@ -15,3 +15,17 @@ var (
 type permutation struct {
 	Seq    []uint8
 	hash   uint64
+	length int
+	i      int
+	j      int
+}
+
+func (p *permutation) String() string {
+	return fmt.Sprint(p.hash, ": ", p.Seq)
+}
+
+// Create a permutation of 1...n from an int slice
+func NewPerm(p []uint8) *permutation {
+	h := hash(p, 0)
+	return &permutation{p, h, len(p), 0, 0}
+}
