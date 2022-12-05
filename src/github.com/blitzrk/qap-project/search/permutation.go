1603900defec0d8199f391f887686a24048c86bc
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-12-05 15:41:16 +0000

    travis integration

--- src/github.com/blitzrk/qap-project/search/permutation.go
+++ src/github.com/blitzrk/qap-project/search/permutation.go
@@ -29,3 +29,17 @@ func NewPerm(p []uint8) *permutation {
 	h := hash(p, 0)
 	return &permutation{p, h, len(p), 0, 0}
 }
+
+// Create random permutation of 1...n
+func RandPerm(n int) *permutation {
+	p := rand.Perm(n)
+	r := make([]uint8, len(p))
+
+	for i, v := range p {
+		r[i] = uint8(v + 1)
+	}
+
+	return NewPerm(r)
+}
+
+// DEPRECATED:
