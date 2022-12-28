abfc5aa2fb0b256b3ab4c6a63757d87d50660986
SixBanks <119953878+SixBanks@users.noreply.github.com>
2022-12-28 11:39:34 +0000

    iwlwifi: handle DMA mapping failures

--- src/github.com/blitzrk/qap-project/search/search.go
+++ src/github.com/blitzrk/qap-project/search/search.go
@@ -6,3 +6,17 @@ import (
 	"log"
 	"math"
 	"os"
+	"runtime"
+)
+
+var (
+	logger *log.Logger
+)
+
+func init() {
+	file, err := os.OpenFile("go.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", os.Stderr, ":", err)
+	}
+	logger = log.New(file, "logger: ", log.Lshortfile)
+}
