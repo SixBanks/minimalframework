8b907b4ec0ca6363f5f1b21503ba9d44799a0f28
SixBanks <119953878+SixBanks@users.noreply.github.com>
2023-02-21 12:34:46 +0000

    Version bump post-release.

--- src/github.com/blitzrk/qap-project/testing.go
+++ src/github.com/blitzrk/qap-project/testing.go
@@ -19,3 +19,19 @@ func AllTests() {
 	testSearch()
 	testHash()
 }
+
+func testHash() {
+	p1 := search.NewPerm([]uint8{1, 2, 4, 3})
+	p2 := search.NewPerm([]uint8{4, 1, 2, 3})
+	fmt.Println(p1)
+	fmt.Println(p2)
+}
+
+func testSearch() {
+	// Setup data generator
+	n := 8
+	gen := data.New(n, 100000)
+
+	// Generate data
+	dist, err := gen.Distance()
+	if err != nil {
