c8c32688cacf83b13786ab193050d22a5089d67d
SixBanks <119953878+SixBanks@users.noreply.github.com>
2023-03-04 14:48:06 +0000

    Removed print statements.

--- src/github.com/blitzrk/qap-project/testing.go
+++ src/github.com/blitzrk/qap-project/testing.go
@@ -116,3 +116,16 @@ func testQAPLIBData() {
 	data, err := readDat("bur26a.dat")
 	if err != nil {
 		fmt.Println(err)
+		return
+	}
+
+	n := data[0][0][0]
+	times := data[1]
+	freqs := data[2]
+	totalF := freqs.Sum()
+
+	fmt.Println(n)
+	fmt.Println()
+	fmt.Println(times)
+	fmt.Println()
+	fmt.Println(freqs)
